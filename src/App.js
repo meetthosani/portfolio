36e319c695fbbf51339ddcd84b0b1ccf3738e6bb
meetthosani <meet.thosani@gmail.com>
2023-10-13 20:42:20 -0400

    fixing stupid git issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { About, Header, Footer, Skills, Testimonial, Work } from "./container";
+import { About, Footer, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 import "./App.scss";
 
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="app">
       <Navbar />
-      <Header />
+
       <About />
       <Work />
       <Skills />
