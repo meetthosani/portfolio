7a8138955546f551e6b31906918bd3cf12b93680
meetthosani <meet.thosani@gmail.com>
2023-10-16 16:22:36 -0400

    fixed sm icons and nav dots

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -2,7 +2,7 @@ import { NavigationDots, SocialMedia } from "../components";
 function AppWrap(Component, idName, classNames) {
   return function HOC() {
     return (
-      <div id={idName} className={`app_container ${classNames}`}>
+      <div id={idName} className={`app__container ${classNames}`}>
         <SocialMedia />
         <div className="app__wrapper app__flex">
           <Component />
